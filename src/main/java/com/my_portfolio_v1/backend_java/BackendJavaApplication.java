d784afbd2398369b53eb30ff6daa8fe1d9e9ef73
Shubham Kanani <shubham@shubhams-MacBook-Pro.local>
2026-04-01 12:45:33 +0530

    My Portfolio Backend initialized

---
+++ src/main/java/com/my_portfolio_v1/backend_java/BackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.my_portfolio_v1.backend_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my_portfolio_v1/backend_java/BackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_portfolio_v1.backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
